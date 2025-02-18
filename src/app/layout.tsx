482f34eda42b9ac36795f13fa00ef31e45ec3894
jhonatan-oliveiradev <jhonatan.oliveira931002@gmail.com>
2025-02-18 11:45:52 -0300

    :wrench: chore: adjust settings.json to auto format imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,23 +4,23 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 
 const poppins = Poppins({
-	weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-	subsets: ["latin"]
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-	title: "FSW Donalds",
-	description: "Bora finalizar esse projeto lindo!"
+  title: "FSW Donalds",
+  description: "Bora finalizar esse projeto lindo!",
 };
 
 export default function RootLayout({
-	children
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="pt-br">
-			<body className={`${poppins.className} antialiased`}>{children}</body>
-		</html>
-	);
+  return (
+    <html lang="pt-br">
+      <body className={`${poppins.className} antialiased`}>{children}</body>
+    </html>
+  );
 }
